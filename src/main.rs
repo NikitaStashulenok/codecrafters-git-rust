33db9564dc07b3da1d84e70f0597b95324114c08
Nikita Stashulenok <nikita.stashulenok@resliv.by>
2026-04-06 16:30:14 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use anyhow::Context;
 use clap::{Parser, Subcommand};
 use std::fs;
 use std::path::PathBuf;
@@ -85,40 +84,7 @@ fn main() -> anyhow::Result<()> {
             tree_hash,
             parent_hash,
         } => commands::commit_tree::invoke(message, tree_hash, parent_hash)?,
-        Command::Clone { url, dir } => {
-            fn invoke(repository: String, directory: Option<String>) -> anyhow::Result<()> {
-                let url = if repository.starts_with("https://") || repository.starts_with("http://")
-                {
-                    repository
-                } else {
-                    format!("https://{repository}")
-                };
-
-                let output_dir = if let Some(dir) = directory {
-                    dir
-                } else {
-                    let repo_name = url
-                        .rsplit('/')
-                        .next()
-                        .and_then(|s| s.strip_suffix(".git"))
-                        .unwrap_or("repository");
-                    repo_name.to_string()
-                };
-
-                println!("Cloning from {url} into {output_dir}");
-
-                let status = std::process::Command::new("git")
-                    .args(&["clone", &url, &output_dir])
-                    .status()
-                    .context("invoke git clone")?;
-
-                anyhow::ensure!(status.success(), "git clone failed");
-
-                Ok(())
-            }
-
-            invoke(url, dir)?;
-        }
+        Command::Clone { url, dir } => commands::clone::invoke(url, dir)?,
     }
 
     Ok(())
